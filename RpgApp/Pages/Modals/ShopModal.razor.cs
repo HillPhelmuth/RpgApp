8845ebe760c1c9113efb99ce63ef47f7ac56ec9b
Dakota <dakota.howard91@gmail.com>
2021-02-27 11:49:33 -0600

    WIP ShopModal images

--- RpgApp/Pages/Modals/ShopModal.razor.cs
+++ RpgApp/Pages/Modals/ShopModal.razor.cs
@@ -11,6 +11,7 @@ using RpgApp.Shared;
 using RpgApp.Shared.Types;
 using RpgApp.Shared.Types.Enums;
 using RpgApp.Shared.Types.PlayerExtensions;
+using RpgComponentLibrary.Services;
 
 namespace RpgApp.Client.Pages.Modals
 {
@@ -20,7 +21,22 @@ namespace RpgApp.Client.Pages.Modals
         private List<KeyValuePair<string, Equipment>> _imagesEquipPairs = new();
         private List<KeyValuePair<string, Equipment>> AddImages(List<Equipment> equipment)
         {
-            return equipment.Select(eq => eq.AddImagePath()).ToList();
+            int id = 0;
+            List<KeyValuePair<string, Equipment>> ImageItems = new List<KeyValuePair<string, Equipment>>();
+            foreach (var item in equipment)
+            {
+                if (item.Name == "Dagger")
+                {
+                    id = 9;
+                }
+                if (item.Name == "Short sword")
+                {
+                    id = 1;
+                }
+                else { id = 5; }
+                ImageItems.Add(TestImageAdd(item, id));
+            }
+            return ImageItems.ToList();
         }
         [Inject]
         public AppStateManager AppState { get; set; }
@@ -31,7 +47,7 @@ namespace RpgApp.Client.Pages.Modals
         {
             var apiResponse = await HttpClient.GetFromJsonAsync<List<Equipment>>($"{AppConstants.ApiUrl}/GetSomeEquipment?goldMax={30}");
             shopInventory = apiResponse;
-            _imagesEquipPairs = AddImages(AppState.AllEquipment);
+            _imagesEquipPairs = AddImages(shopInventory);
             foreach (var item in shopInventory.Where(item => item.Effects == null))
             {
                 item.Effects = new List<Effect> { new Effect { Type = EffectType.Status, Value = "none" } };
@@ -41,6 +57,10 @@ namespace RpgApp.Client.Pages.Modals
         {
             AppState.CurrentPlayer.Inventory.Add(equipment);
             await HttpClient.PostAsJsonAsync($"{AppConstants.ApiUrl}/UpdateOrAddPlayer", AppState.CurrentPlayer);
+        }
+        private KeyValuePair<string, Equipment> TestImageAdd(Equipment equipment, int id)
+        {
+            return new KeyValuePair<string, Equipment>(ImageData.IndexedImages[id], equipment);
         }
     }
 }
